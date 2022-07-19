52d7ed5e62315abbcab49e55efcc1fbec35b8aad
JoaoPaulo02438 <joao.paulo02438@gmail.com>
2022-07-19 13:05:25 -0300

    flexible notification

--- lib/Components/home/notifications/notifications_home.dart
+++ lib/Components/home/notifications/notifications_home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class NotificationHome extends StatelessWidget {
@@ -5,289 +7,161 @@ class NotificationHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Wrap(
-      children: [
-        //1 Container
-        Container(
-          child: Column(
-            children: [
-              //1 linha
-              Row(
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  const Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Align(
-                      alignment: Alignment.topLeft,
-                      child: CircleAvatar(
-                        backgroundColor: Colors.white,
-                        backgroundImage: NetworkImage(
-                            'https://www.woolha.com/media/2020/03/eevee.png'),
-                        radius: 25,
+    return Wrap(children: [
+      Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * .25,
+                  height: MediaQuery.of(context).size.height * .25,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 3,
+                        offset: Offset(1, 1),
                       ),
-                    ),
+                    ],
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      child: Text(
-                        'João Paulo Bezerra \nDesenvolvimento',
-                        style: TextStyle(
-                          fontSize: 12,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            backgroundImage: NetworkImage(
+                                'https://www.woolha.com/media/2020/03/eevee.png'),
+                            radius: 25,
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Align(
-                      //alignment: Alignment.topRight,
-                      child: IconButton(
-                        alignment: Alignment.bottomRight,
-                        icon: Icon(
-                          Icons.more_horiz,
-                        ),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              //2 linha
-              Row(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      child: Text(
-                        'Bem-vindos ao Portal Tools, confira as \nnovidades na aba do menu superior.',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              //3 linha
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Align(
-                      alignment: Alignment.topRight,
-                      child: Text(
-                        'postado ás 12:26 18/07/2022',
-                        style: TextStyle(fontSize: 8),
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-          width: MediaQuery.of(context).size.width * .25,
-          height: MediaQuery.of(context).size.height * .25,
-          decoration: BoxDecoration(
-            color: Color.fromRGBO(255, 255, 255, 1),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 3,
-                blurRadius: 3,
-                offset: Offset(1, 1),
-              ),
-            ],
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-        SizedBox(width: 10),
-        //2 Container
-        Container(
-          child: Column(
-            children: [
-              //1 linha
-              Row(
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  const Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Align(
-                      alignment: Alignment.topLeft,
-                      child: CircleAvatar(
-                        backgroundColor: Colors.white,
-                        backgroundImage: NetworkImage(
-                            'https://www.woolha.com/media/2020/03/eevee.png'),
-                        radius: 25,
-                      ),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      child: Text(
-                        'Cleiton Jr. \nDesenvolvimento',
-                        style: TextStyle(
-                          fontSize: 12,
+                      Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Text(
+                            'João Paulo\nDesenvolvimento',
+                            style: TextStyle(
+                              fontSize: 10,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                      // ignore: prefer_const_constructors
+
+                      // ignore: prefer_const_constructors
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Align(
-                      //alignment: Alignment.topRight,
-                      child: IconButton(
-                        alignment: Alignment.bottomRight,
-                        icon: Icon(
-                          Icons.more_horiz,
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: MediaQuery.of(context).size.width * .25,
+                  height: MediaQuery.of(context).size.height * .25,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 3,
+                        offset: Offset(1, 1),
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            backgroundImage: NetworkImage(
+                                'https://www.woolha.com/media/2020/03/eevee.png'),
+                            radius: 25,
+                          ),
                         ),
-                        onPressed: () {},
                       ),
-                    ),
+                      Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Text(
+                            'João Paulo\nDesenvolvimento',
+                            style: TextStyle(
+                              fontSize: 10,
+                            ),
+                          ),
+                        ),
+                      ),
+                      // ignore: prefer_const_constructors
+
+                      // ignore: prefer_const_constructors
+                    ],
                   ),
-                ],
-              ),
-              //2 linha
-              Row(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      child: Text(
-                        'Segue manual para utilização das \nferramentas do portal.',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: MediaQuery.of(context).size.width * .25,
+                  height: MediaQuery.of(context).size.height * .25,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 3,
+                        offset: Offset(1, 1),
                       ),
-                    ),
+                    ],
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                ],
-              ),
-              //3 linha
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Align(
-                      alignment: Alignment.topRight,
-                      child: Text(
-                        'postado ás 12:26 18/07/2022',
-                        style: TextStyle(fontSize: 8),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            backgroundImage: NetworkImage(
+                                'https://www.woolha.com/media/2020/03/eevee.png'),
+                            radius: 25,
+                          ),
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              )
-            ],
+                      Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Text(
+                            'João Paulo\nDesenvolvimento',
+                            style: TextStyle(
+                              fontSize: 10,
+                            ),
+                          ),
+                        ),
+                      ),
+                      // ignore: prefer_const_constructors
+
+                      // ignore: prefer_const_constructors
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-          width: MediaQuery.of(context).size.width * .25,
-          height: MediaQuery.of(context).size.height * .25,
-          decoration: BoxDecoration(
-            color: Color.fromRGBO(255, 255, 255, 1),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 3,
-                blurRadius: 3,
-                offset: Offset(1, 1),
-              ),
-            ],
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-        SizedBox(width: 10),
-        Container(
-          child: Column(
-            children: [
-              //1 linha
-              Row(
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      backgroundImage: NetworkImage(
-                          'https://www.woolha.com/media/2020/03/eevee.png'),
-                      radius: 25,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    'Douglas Bi. \nDesenvolvimento',
-                    style: TextStyle(
-                      fontSize: 12,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 120,
-                  ),
-                  IconButton(
-                    alignment: Alignment.topLeft,
-                    icon: Icon(
-                      Icons.more_horiz,
-                    ),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-              //2 linha
-              Row(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      child: Text(
-                        'Segue manual para utilização das \nferramentas do portal.',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              //3 linha
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: Text(
-                        'postado ás 12:26 18/07/2022',
-                        style: TextStyle(fontSize: 8),
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-          width: MediaQuery.of(context).size.width * .25,
-          height: MediaQuery.of(context).size.height * .25,
-          decoration: BoxDecoration(
-            color: Color.fromRGBO(255, 255, 255, 1),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 3,
-                blurRadius: 3,
-                offset: Offset(1, 1),
-              ),
-            ],
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-      ],
-    );
+        ],
+      ),
+    ]);
   }
 }
